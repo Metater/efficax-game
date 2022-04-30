fdd5dbb75b7512a4ee4c651eba1975fe68a57490
Connor <61102529+Metater@users.noreply.github.com>
2022-04-30 02:01:59 -0400

    work

--- efficax-server/metaitus/src/entity.rs
+++ efficax-server/metaitus/src/entity.rs
@@ -4,7 +4,7 @@ use super::{physics::collider::PhysicsCollider};
 
 #[derive(Debug)]
 pub struct MetaitusEntity {
-    pub id: u32,
+    pub id: u64,
     pub pos: Vector2<f32>,
     pub current_cell_index: u32,
 
@@ -19,7 +19,7 @@ pub struct MetaitusEntity {
 
     pub has_repulsion_radius: bool,
     pub repulsion_radius: f32,
-    max_repulsion_mag: f32,
+    pub max_repulsion_mag: f32,
     pub repulsion: f32,
 
     has_drag: bool,
@@ -33,7 +33,7 @@ pub struct MetaitusEntity {
 }
 
 impl MetaitusEntity {
-    pub fn new(id: u32, pos: Vector2<f32>, current_cell_index: u32) -> Self {
+    pub fn new(id: u64, pos: Vector2<f32>, current_cell_index: u32) -> Self {
         MetaitusEntity {
             id,
             pos,
@@ -103,7 +103,7 @@ impl MetaitusEntity {
         self.vel += force * delta_time;
     }
 
-    pub fn tick(&mut self, tick_id: u64, delta_time: f32, near_statics: &Vec<PhysicsCollider>, repulsable_entities: &Vec<(Vector2<f32>, f32, f32)>) -> bool {
+    pub fn tick(&mut self, tick_id: u64, delta_time: f32, near_statics: &Vec<PhysicsCollider>) -> bool {
         self.moved_xy = false;
         
         if self.has_vel_epsilon {
@@ -124,31 +124,6 @@ impl MetaitusEntity {
         }
         else {
             // cannot move, velocity is zero
-        }
-
-        if repulsable_entities.len() > 0 {
-            // if too much repulsion, use avg of vectors again
-            //let mut repulsion_vectors_sum = Vector2::zero();
-            for (entity_pos, entity_repulsion_radius, entity_repulsion) in repulsable_entities {
-                let diff_x = entity_pos.x - self.pos.x;
-                let diff_y = entity_pos.y - self.pos.y;
-                let sqr_distance = (diff_x * diff_x) + (diff_y * diff_y);
-                let radius_sum = entity_repulsion_radius + self.repulsion_radius;
-                let sqr_radius_sum = radius_sum * radius_sum;
-                if sqr_radius_sum > sqr_distance {
-                    let mut repulsion_vector = Vector2::new(0.0, 1.0);
-                    if !sqr_distance.is_zero() {
-                        let mut repulsion_mag = (entity_repulsion + self.repulsion) * (1.0 / sqr_distance);
-                        repulsion_mag = repulsion_mag.clamp(-self.max_repulsion_mag, self.max_repulsion_mag);
-                        let distance = sqr_distance.sqrt();
-                        repulsion_vector = -Vector2::new(diff_x / distance, diff_y / distance) * repulsion_mag;
-                    }
-                    //repulsion_vectors_sum += repulsion_vector;
-                    self.add_force(repulsion_vector, delta_time);
-                }
-            }
-            //let average_repulsion_vector = repulsion_vectors_sum / repulsable_entities.len() as f32;
-            //self.add_force(average_repulsion_vector, delta_time);
         }
 
         self.tick_count += 1;
--- efficax-server/metaitus/src/zone.rs
+++ efficax-server/metaitus/src/zone.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use cgmath::{Vector2};
+use cgmath::{Vector2, Zero};
 
 use efficax_utils::id_gen::IdGen;
 
@@ -38,19 +38,94 @@ impl MetaitusZone {
         }
     }
 
+    pub fn tick(&mut self, tick_id: u64, delta_time: f32) {
+
+        // near_statics, repulsion_vector
+        let cached_entity_data = self.calculate_entity_data();
+
+        for (i, entity) in self.entities.values_mut().enumerate() {
+            let (near_statics, repulsion_vector) = &cached_entity_data[i];
+            entity.add_force(*repulsion_vector, delta_time);
+            let moved_xy = entity.tick(tick_id, delta_time, &near_statics);
+            if moved_xy {
+                let last_cell_index = entity.current_cell_index;
+                entity.current_cell_index = Self::get_index_at_pos(entity.pos);
+                if last_cell_index != entity.current_cell_index {
+                    let last_cell = self.cells.get_mut(&last_cell_index).unwrap();
+                    last_cell.retain(|&id| id != entity.id);
+                    if last_cell.len() == 0 {
+                        self.cells.remove(&last_cell_index);
+                    }
+                    let current_cell = self.cells.entry(entity.current_cell_index).or_insert_with(Vec::new);
+                    current_cell.push(entity.id);
+                }
+            }
+        }
+    }
+
+    fn calculate_entity_data(&self) -> Vec<(Vec<PhysicsCollider>, Vector2<f32>)> {
+        let mut cached_entity_data = Vec::new();
+
+        for entity in self.entities.values() {
+            let cell_indicies = self.get_cell_and_surrounding_indicies(entity.pos);
+            let mut near_statics = Vec::new();
+            let mut repulsion_vector = Vector2::zero();
+            for cell_index in &cell_indicies {
+                // find nearby statics
+                if let Some(cell_statics) = self.statics.get(cell_index) {
+                    for &cell_static in cell_statics {
+                        near_statics.push(cell_static);
+                    }
+                }
+
+                // find nearby entities to repulse
+                if let Some(entities) = self.cells.get(cell_index) {
+                    for entity_id in entities {
+                        if let Some(near_entity) = self.entities.get(entity_id) {
+                            if near_entity.id == entity.id || !near_entity.has_repulsion_radius {
+                                continue
+                            }
+                            repulsion_vector += Self::get_repulsion_force(entity, near_entity);
+                        }
+                    }
+                }
+            }
+            cached_entity_data.push((near_statics, repulsion_vector));
+        }
+
+        cached_entity_data
+    }
+
+    fn get_repulsion_force(entity: &MetaitusEntity, other: &MetaitusEntity) -> Vector2<f32> {
+        let diff_x = other.pos.x - entity.pos.x;
+        let diff_y = other.pos.y - entity.pos.y;
+        let sqr_distance = (diff_x * diff_x) + (diff_y * diff_y);
+        let center_distance = other.repulsion_radius + entity.repulsion_radius;
+        if center_distance * center_distance > sqr_distance {
+            if !sqr_distance.is_zero() {
+                let mut repulsion_mag = (other.repulsion + entity.repulsion) * (1.0 / sqr_distance);
+                repulsion_mag = repulsion_mag.clamp(-entity.max_repulsion_mag, entity.max_repulsion_mag);
+                let distance = sqr_distance.sqrt();
+
+                -Vector2::new(diff_x / distance, diff_y / distance) * repulsion_mag;
+            }
+        }
+        Vector2::zero()
+    }
+
     fn get_cell_and_surrounding_indicies(&self, pos: Vector2<f32>) -> Vec<u32> {
         let mut cell_indicies = Vec::with_capacity(9);
         let int_coords = Self::get_int_coords_at_pos(pos);
         let index = Self::get_index_at_int_coords(int_coords);
         cell_indicies.push(index);
-        cell_indicies.push(index - Self::DIMENSION_CELL_LENGTH); // n
-        cell_indicies.push(index + Self::DIMENSION_CELL_LENGTH); // s
-        cell_indicies.push(index + 1); // e
-        cell_indicies.push(index - 1); // w
-        cell_indicies.push((index - Self::DIMENSION_CELL_LENGTH) + 1); // ne
-        cell_indicies.push((index - Self::DIMENSION_CELL_LENGTH) - 1); // nw
-        cell_indicies.push((index + Self::DIMENSION_CELL_LENGTH) + 1); // se
-        cell_indicies.push((index + Self::DIMENSION_CELL_LENGTH) - 1); // sw
+        cell_indicies.push(index - Self::DIMENSION_CELL_LENGTH);
+        cell_indicies.push(index + Self::DIMENSION_CELL_LENGTH);
+        cell_indicies.push(index + 1);
+        cell_indicies.push(index - 1);
+        cell_indicies.push((index - Self::DIMENSION_CELL_LENGTH) + 1);
+        cell_indicies.push((index - Self::DIMENSION_CELL_LENGTH) - 1);
+        cell_indicies.push((index + Self::DIMENSION_CELL_LENGTH) + 1);
+        cell_indicies.push((index + Self::DIMENSION_CELL_LENGTH) - 1);
         cell_indicies
     }
 }
@@ -78,5 +153,10 @@ impl MetaitusZone {
         let x = ((index as f32 % Self::DIMENSION_CELL_LENGTH as f32) - Self::HALF_DIMENSION_CELL_LENGTH as f32) * Self::CELL_SIZE as f32;
         let y = ((index as f32 / Self::DIMENSION_CELL_LENGTH as f32) - Self::HALF_DIMENSION_CELL_LENGTH as f32) * Self::CELL_SIZE as f32;
         return Vector2::new(x, y)
+    }
+}
+
+impl MetaitusZone {
+    pub fn add_cell_static(&mut self, index: u32, entity_id: u32) {
     }
 }
